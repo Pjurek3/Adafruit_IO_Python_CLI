bb2126d6be5e11a27139ce1dc82a59d1dbcebc31
Paul <pjurek3@gmail.com>
2022-12-24 15:04:39 -0800

    formatted with black

--- adafruit_io_cli/main.py
+++ adafruit_io_cli/main.py
@@ -12,51 +12,55 @@ app = typer.Typer()
 
 console = rich.console.Console()
 
-def get_feed_data(feed:str):
+
+def get_feed_data(feed: str):
     """returns the feeddata from online"""
-    key = os.environ['ADAFRUIT_IO_KEY']
-    user = os.environ['ADAFRUIT_IO_USERNAME']
-    
-    headers = {'X-AIO-Key': key}
-    url = f'https://io.adafruit.com/api/v2/{user}/feeds/{feed}/data/last'
+    key = os.environ["ADAFRUIT_IO_KEY"]
+    user = os.environ["ADAFRUIT_IO_USERNAME"]
+
+    headers = {"X-AIO-Key": key}
+    url = f"https://io.adafruit.com/api/v2/{user}/feeds/{feed}/data/last"
 
     r = requests.get(url, headers=headers)
-    
-    # this section formats time to the Seattle time.  Expected time from the 
+
+    # this section formats time to the Seattle time.  Expected time from the
     # data feed is UTC
     date_format = "%Y-%m-%dT%H:%M:%SZ"
-    utc_time = datetime.datetime.strptime(r.json()['created_at'], date_format)
+    utc_time = datetime.datetime.strptime(r.json()["created_at"], date_format)
     seattle_time = utc_time - datetime.timedelta(hours=8)
 
-    return {'timestamp': seattle_time,
-            'value': r.json()['value']
-            }
+    return {"timestamp": seattle_time, "value": r.json()["value"]}
+
 
 def get_humidity_data():
     """retrieves the humidity data from online"""
-    feed = 'office-temperature.office-humidity'
+    feed = "office-temperature.office-humidity"
     return get_feed_data(feed=feed)
+
 
 def get_temperature_data():
     """retrieves the temperature data from online"""
-    feed = 'office-temperature.office-temperature'
+    feed = "office-temperature.office-temperature"
     return get_feed_data(feed=feed)
+
 
 def get_air_quality_pm10_data():
     """returns all the air quality data feeds"""
-    feed = 'air-quality-pm10'
+    feed = "air-quality-pm10"
     return get_feed_data(feed=feed)
+
 
 def get_air_quality_pm25_data():
     """returns all the air quality data feeds"""
-    feed = 'air-quality-pm25'
+    feed = "air-quality-pm25"
     return get_feed_data(feed=feed)
+
 
 def get_air_quality_pm100_data():
     """returns all the air quality data feeds"""
-    feed = 'air-quality-pm100'
+    feed = "air-quality-pm100"
     return get_feed_data(feed=feed)
-    
+
 
 @app.callback()
 def callback():
@@ -64,6 +68,7 @@ def callback():
     Awesome Portal Gun
     """
     pass
+
 
 @app.command()
 def stats():
@@ -78,14 +83,13 @@ def stats():
         aq25 = get_air_quality_pm25_data()
         aq100 = get_air_quality_pm100_data()
 
-        
     print(f'Readings at {temperature_data["timestamp"]}')
-    print('-------------------------------------------------------')
+    print("-------------------------------------------------------")
     print(f'temperature: {float(temperature_data["value"]):.1f} C')
     print(f'humidity: {float(humidity_data["value"]):.1f} %')
     print()
     print(f'Air Quality Readings at {aq10["timestamp"]}')
-    print('-------------------------------------------------------')
+    print("-------------------------------------------------------")
     print(f'pm10: {float(aq10["value"])}')
     print(f'pm25: {float(aq25["value"])}')
     print(f'pm100: {float(aq100["value"])}')
