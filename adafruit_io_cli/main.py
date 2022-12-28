387b1d1164a6130403c3a249d35b95e22ee88b1a
Paul <pjurek3@gmail.com>
2022-12-28 06:33:11 -0800

    messed with changelog and test

--- adafruit_io_cli/main.py
+++ adafruit_io_cli/main.py
@@ -5,10 +5,8 @@ from typing import List
 
 import dotenv
 import httpx
-import requests
 import rich
 import typer
-from rich.console import Console
 from rich.table import Table
 
 dotenv.load_dotenv()
@@ -30,130 +28,6 @@ def date_diff(d1: str) -> int:
     delta = d2 - d1
 
     return delta.days * 24 * 60 + int(delta.seconds / 60)
-
-
-def get_feed_data(feed: str):
-    """returns the feeddata from online"""
-    key = os.environ["ADAFRUIT_IO_KEY"]
-    user = os.environ["ADAFRUIT_IO_USERNAME"]
-
-    headers = {"X-AIO-Key": key}
-    url = f"https://io.adafruit.com/api/v2/{user}/feeds/{feed}/data/last"
-
-    r = requests.get(url, headers=headers)
-
-    # this section formats time to the Seattle time.  Expected time from the
-    # data feed is UTC
-
-    utc_time = datetime.datetime.strptime(r.json()["created_at"], date_format)
-    seattle_time = utc_time - datetime.timedelta(hours=8)
-
-    return {"timestamp": seattle_time, "value": r.json()["value"]}
-
-
-async def get_feed_data_async(feed: str, client: httpx.Client):
-    """returns the feeddata from online"""
-    key = os.environ["ADAFRUIT_IO_KEY"]
-    user = os.environ["ADAFRUIT_IO_USERNAME"]
-
-    headers = {"X-AIO-Key": key}
-    url = f"https://io.adafruit.com/api/v2/{user}/feeds/{feed}/data/last"
-
-    r = await client.get(url, headers=headers)
-
-    # this section formats time to the Seattle time.  Expected time from the
-    # data feed is UTC
-
-    utc_time = datetime.datetime.strptime(r.json()["created_at"], date_format)
-    seattle_time = utc_time - datetime.timedelta(hours=8)
-
-    return {"timestamp": seattle_time, "value": r.json()["value"]}
-
-
-async def get_async_sensor_data():
-    """async method to get all the data for the async methods"""
-    async with httpx.AsyncClient() as client:
-        r_humidity = await get_feed_data_async(
-            feed="office-temperature.office-humidity", client=client
-        )
-        r_temperature = await get_feed_data_async(
-            feed="office-temperature.office-temperature", client=client
-        )
-        r_pm10 = await get_feed_data_async(feed="air-quality-pm10", client=client)
-        r_pm25 = await get_feed_data_async(feed="air-quality-pm25", client=client)
-        r_pm100 = await get_feed_data_async(feed="air-quality-pm100", client=client)
-
-    return {
-        "humidity": r_humidity,
-        "temperature": r_temperature,
-        "r_pm10": r_pm10,
-        "r_pm25": r_pm25,
-        "r_pm100": r_pm100,
-    }
-
-
-def get_full_feed_data(feed: str):
-    """returns the feeddata from online"""
-    key = os.environ["ADAFRUIT_IO_KEY"]
-    user = os.environ["ADAFRUIT_IO_USERNAME"]
-
-    headers = {"X-AIO-Key": key}
-    url = f"https://io.adafruit.com/api/v2/{user}/feeds/{feed}/data"
-
-    # set limit to 1000 so we get full data if possible
-    # this is current limit of the API
-    start_time = (datetime.datetime.utcnow() - datetime.timedelta(hours=8)).strftime(
-        date_format
-    )
-    r = requests.get(
-        url, headers=headers, params={"limit": 1000, "start_time": start_time}
-    )
-
-    # this section formats time to the Seattle time.  Expected time from the
-    # data feed is UTC
-
-    query_data = r.json()
-    results = []
-    for record in query_data:
-        utc_time = datetime.datetime.strptime(record["created_at"], date_format)
-        results.append(
-            {
-                "timestamp": utc_time - datetime.timedelta(hours=8),
-                "value": float(record["value"]),
-            }
-        )
-
-    return results
-
-
-def get_humidity_data():
-    """retrieves the humidity data from online"""
-    feed = "office-temperature.office-humidity"
-    return get_feed_data(feed=feed)
-
-
-def get_temperature_data():
-    """retrieves the temperature data from online"""
-    feed = "office-temperature.office-temperature"
-    return get_feed_data(feed=feed)
-
-
-def get_air_quality_pm10_data():
-    """returns all the air quality data feeds"""
-    feed = "air-quality-pm10"
-    return get_feed_data(feed=feed)
-
-
-def get_air_quality_pm25_data():
-    """returns all the air quality data feeds"""
-    feed = "air-quality-pm25"
-    return get_feed_data(feed=feed)
-
-
-def get_air_quality_pm100_data():
-    """returns all the air quality data feeds"""
-    feed = "air-quality-pm100"
-    return get_feed_data(feed=feed)
 
 
 @app.callback()
@@ -252,52 +126,6 @@ class SensorSuite:
 
 @app.command()
 def stats():
-    """
-    Shoot the portal gun
-    """
-    console.clear()
-    with console.status("[bold green]Getting Data...") as status:
-        temperature_data = get_temperature_data()
-        humidity_data = get_humidity_data()
-        aq10 = get_air_quality_pm10_data()
-        aq25 = get_air_quality_pm25_data()
-        aq100 = get_air_quality_pm100_data()
-
-    print(f'Readings at {temperature_data["timestamp"]}')
-    print(
-        f'Minutes since last reading: {date_diff(temperature_data["timestamp"])} minutes'
-    )
-
-    table = Table()
-    table.add_column("measure", justify="right")
-    table.add_column("value")
-    table.add_column("min-value")
-    table.add_column("max-value")
-
-    table.add_row("temperature", f'{float(temperature_data["value"]):.1f} C')
-    table.add_row("humidity", f'{float(humidity_data["value"]):.1f} %')
-    table.add_row("pm10", str(float(aq10["value"])))
-    table.add_row("pm25", str(float(aq25["value"])))
-    table.add_row("pm100", str(float(aq100["value"])))
-
-    console.print(table)
-
-
-@app.command()
-def stats_2():
-    """
-    Shoot the portal gun
-    """
-    console.clear()
-    loop = asyncio.get_event_loop()
-    tasks = (get_async_sensor_data(), get_async_sensor_data(), get_async_sensor_data())
-    results = loop.run_until_complete(asyncio.gather(*tasks))
-    rich.print(results)
-    loop.close()
-
-
-@app.command()
-def stats_3():
     """
     Shoot the portal gun
     """
--- adafruit_io_cli/tests/test_performance.py
+++ adafruit_io_cli/tests/test_performance.py
@@ -1,14 +1,9 @@
 """performance test for accessing the data from API"""
-from ..main import stats, stats_2
+from ..main import stats, Sensor, SensorSuite
+
+import asyncio
 
 
 def test_all_feed_data(benchmark):
     """tests performance to build data object which contains all items"""
-    result = benchmark(stats)
-    assert True
-
-
-def test_all_feed_data_async(benchmark):
-    """tests performance to build data object which contains all items"""
-    result = benchmark(stats_2)
     assert True
